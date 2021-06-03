aa88935ddc5cfaa7aa41c75c7ad57a41f2076286
BREYZACK <85296678+BREYZACK@users.noreply.github.com>
2021-06-03 16:02:11 +0200

    Initial commit

---
+++ MediaTek86/modele/Responsable.cs
@@ -0,0 +1,23 @@
+﻿namespace MediaTek86.modele
+{
+    /// <summary>
+    /// Création d'un responsable
+    /// </summary>
+    public class Responsable
+    {
+        // Déclaration des propriétés
+        private string login;
+        private string pwd;
+
+        /// <summary>
+        /// Constructeur : valorise les propriétés
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="pwd"></param>
+        public Responsable(string login, string pwd)
+        {
+            this.login = login;
+            this.pwd = pwd;
+        }
+    }
+}
