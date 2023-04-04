1060a9407fde7c87c9d4d7b11292921792a6eaa6
BREYZACK <scam.steam666@gmail.com>
2023-04-04 20:23:36 +0200

    TheMediatak86

---
+++ MediaTek86/vue/SeConnecter.cs
@@ -0,0 +1,47 @@
+﻿using MediaTek86.controle;
+using System;
+using System.Windows.Forms;
+
+namespace MediaTek86.vue
+{
+    public partial class frmSeConnecter : Form
+    {
+        /// <summary>
+        /// instance du controleur
+        /// </summary>
+        private Controle controle;
+
+        /// <summary>
+        /// Initialisation des composants graphiques
+        /// Récupération du controleur
+        /// </summary>
+        public frmSeConnecter(Controle controle)
+        {
+            InitializeComponent();
+            this.controle = controle;
+        }
+
+        /// <summary>
+        /// Permet de se connecter à l'application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSeConnecter_Click(object sender, EventArgs e)
+        {
+            if (!txtLogin.Text.Equals("") && !txtMotDePasse.Text.Equals(""))
+            {
+                if (!controle.ControleAuthentification(txtLogin.Text, txtMotDePasse.Text))
+                {
+                    MessageBox.Show("Authentification incorrecte ou vous n'êtes pas admin", "Alerte");
+                    txtLogin.Text = "";
+                    txtMotDePasse.Text = "";
+                    txtLogin.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tous les champs doivent être remplis.", "Information");
+            }
+        }
+    }
+}
